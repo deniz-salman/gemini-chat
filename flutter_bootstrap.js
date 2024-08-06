af5b11ca2845d23309df8d24935973be33ec7a36
deniz-salman <deniz-salman@users.noreply.github.com>
2024-08-06 18:55:22 +0000

    deploy: f82a88124780b1ac7917235684b3e3b9a76ffbb8

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"55eae6864b296dd9f43b2cc7577ec256e5c32a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4112903772"
+    serviceWorkerVersion: "2502368070"
   }
 });
